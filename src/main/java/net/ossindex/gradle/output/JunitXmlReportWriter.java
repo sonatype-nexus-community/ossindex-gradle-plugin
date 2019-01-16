45cd2caf529e577e2720ed67aac8aa613506bdf9
Ken Duck <kduck@sonatype.com>
2019-01-16 02:25:47 -0400

    Change the report writer to use file locks.

--- src/main/java/net/ossindex/gradle/output/JunitXmlReportWriter.java
+++ src/main/java/net/ossindex/gradle/output/JunitXmlReportWriter.java
@@ -1,5 +1,28 @@
 package net.ossindex.gradle.output;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Attr;
@@ -9,185 +32,259 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.ArrayList;
-import java.util.Set;
+public class JunitXmlReportWriter
+{
+  private static final Logger logger = LoggerFactory.getLogger(JunitXmlReportWriter.class);
 
-public class JunitXmlReportWriter {
-    private static final Logger logger = LoggerFactory.getLogger(JunitXmlReportWriter.class);
+  public static String pathToReport;
 
-    public Integer testCaseId = 0;
-    private boolean skip = false;
+  private boolean skip = false;
+
+  private Element testSuite;
+
+  private Long startSeconds = null;
+
+  public void init(String junitReport) {
+
+    if (junitReport == null) {
+      skip = true;
+      return;
+    }
+
+    this.pathToReport = junitReport;
+    // Metrics
+    setStartTime();
+  }
+
+  public void setStartTime() {
+    startSeconds = java.time.Instant.now().getEpochSecond();
+  }
+
+  public void updateJunitReport(String totals,
+                                String task,
+                                String artifact,
+                                ArrayList<String> currentVulnerabilityList)
+  {
+
+    if (skip) {
+      return;
+    }
+
+    try (DocResource docResource = new DocResource(pathToReport)) {
+      Document doc = docResource.getDocument();
+
+      // Get a new testcase ID
+      Integer testCaseId = getTotalOfElementsByName(doc, "testcase");
+      testCaseId++;
+
+      // Change to empty string for text, add name tag set to
+      Element testCase = addChildElement(doc, testSuite, "testcase", "");
+      addElementAttribute(doc, testCase, "name", task + " - " + totals);
+      addElementAttribute(doc, testCase, "id", testCaseId.toString());
+      Long elapsedTime = java.time.Instant.now().getEpochSecond() - startSeconds;
+      addElementAttribute(doc, testCase, "time", elapsedTime.toString());
+
+      if (artifact != null) {
+        if (!artifact.substring(0, 1).equals("0")) {
+          Element failure = addChildElement(doc, testCase, "failure", buildFailureString(currentVulnerabilityList));
+          addElementAttribute(doc, failure, "message", artifact);
+        }
+      }
+    }
+    catch (IOException e) {
+      logger.error("Exception writing log: " + e);
+    }
+  }
+
+  public void writeXmlReport(String pathToReport) throws Exception {
+    if (skip) {
+      return;
+    }
+
+    try (DocResource docResource = new DocResource(pathToReport)) {
+      Document doc = docResource.getDocument();
+        String testCount = getTotalOfElementsByName(doc, "testcase").toString();
+        modifyElementAttribute(doc, "testsuites", 0, "tests", testCount);
+        String failureCount = getTotalOfElementsByName(doc, "failure").toString();
+        modifyElementAttribute(doc, "testsuites", 0, "failures", failureCount);
+    }
+  }
+
+  private Element addChildElement(Document doc, Element parent, String name, String data) {
+    Element elem = doc.createElement(name);
+    elem.appendChild(doc.createTextNode(data));
+    parent.appendChild(elem);
+    return elem;
+  }
+
+  private void addElementAttribute(Document doc, Element parent, String name, String value) {
+    Attr attr = doc.createAttribute(name);
+    attr.setValue(value);
+    parent.setAttributeNode(attr);
+  }
+
+  private String buildFailureString(ArrayList<String> currentVulnerabilityList) {
+    String failureString = "";
+    for (String tmp : currentVulnerabilityList) {
+      failureString = failureString + tmp + "\n";
+    }
+    return failureString.trim();
+  }
+
+  private Integer getTotalOfElementsByName(Document doc, String name) {
+    return doc.getElementsByTagName(name).getLength();
+  }
+
+  private void modifyElementAttribute(Document doc, String tagName, Integer index, String attrName, String value) {
+    Node target = doc.getElementsByTagName(tagName).item(index);
+    NamedNodeMap attr = target.getAttributes();
+    Node nodeAttr = attr.getNamedItem(attrName);
+    nodeAttr.setTextContent(value);
+  }
+
+  /**
+   * Manage the lock and writing of the report document.
+   *
+   * Every document edit is performed by:
+   *
+   *   1. Locking the file
+   *   2. Creating/Loading the document
+   *   3. Performing the document changes
+   *   4. Writing the file
+   *   5. Unlocking the file
+   *
+   * This is done to ensure that parallel builds can all work on the same report file.
+   */
+  class DocResource implements AutoCloseable
+  {
+
+    private String path;
+
     private Document doc;
-    private DocumentBuilder docBuilder;
-    private DocumentBuilderFactory docFactory;
-    private Element testSuite;
-    private Long startSeconds = null;
 
-    // Object used for document/file locking.
-    private static final Object fileLock = new Object();
+    public DocResource(final String path) {
+      File f = new File(path);
+      this.path = f.getAbsolutePath();
 
-    public void init(String junitReport) {
+      try {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(path, "rw");
+        FileChannel fc = randomAccessFile.getChannel();
 
-        if (junitReport == null) {
-            skip = true;
-            return;
+        try (FileLock fileLock = fc.lock()) {
+          if (fc.size() == 0) {
+            // If this is a new file, then initialize it.
+            doc = createDocument();
+          }
+          else {
+            doc = loadDocument();
+          }
         }
+      }
+      catch (IOException e) {
+        logger.error("Exception writing log", e);
+      }
 
-        docFactory = DocumentBuilderFactory.newInstance();
-        try {
-            docBuilder = docFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        // If report exists then we update it
-        // else we create a new report
-        synchronized (fileLock) { // Make sure we are not reading and writing at the same time
-            Element rootElement = null;
-            File f = new File(junitReport);
-            if (f.exists() && !f.isDirectory()) {
-                try {
-                    doc = docBuilder.parse(junitReport);
-                }
-                catch (SAXException | IOException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-                testSuite = (Element) doc.getElementsByTagName("testsuite").item(0);
-                testCaseId = getTotalOfElementsByName("testcase");
-            }
-            else {
-                doc = docBuilder.newDocument();
-                rootElement = doc.createElement("testsuites");
-                doc.appendChild(rootElement);
-                addElementAttribute(rootElement, "id", "1");
-                addElementAttribute(rootElement, "failures", "0");
-                addElementAttribute(rootElement, "tests", "0");
-                // Top level test suite
-                testSuite = addChildElement(rootElement, "testsuite", "");
-                addElementAttribute(testSuite, "id", "1");
-                addElementAttribute(testSuite, "name", "OSSIndex");
-            }
-        }
-
-        // Metrics
-        setStartTime();
     }
 
-    public void setStartTime() {
-        startSeconds = java.time.Instant.now().getEpochSecond();
+    public Document getDocument() {
+      return doc;
     }
 
-    public void updateJunitReport(String totals, String task, String artifact, ArrayList<String> currentVulnerabilityList) {
-
-        if (skip) {
-            return;
-        }
-
-        // Change to empty string for text, add name tag set to
-        Element testCase = addChildElement(testSuite, "testcase", "");
-        addElementAttribute(testCase, "name", task + " - " + totals);
-        addElementAttribute(testCase, "id", (testCaseId += 1).toString());
-        Long elapsedTime = java.time.Instant.now().getEpochSecond() - startSeconds;
-        addElementAttribute(testCase, "time", elapsedTime.toString());
-
-        if (artifact != null) {
-            if (! artifact.substring(0, 1).equals("0")) {
-                Element failure = addChildElement(testCase, "failure", buildFailureString(currentVulnerabilityList));
-                addElementAttribute(failure, "message", artifact);
-            }
-        }
+    @Override
+    public void close() throws IOException {
+      writeDocument(doc);
     }
 
-    public void writeXmlReport(String pathToReport) throws Exception {
-        if (skip) {
-            return;
-        }
+    /**
+     * Called only if the file does not exist. Creates the basic document structure and writes to the file.
+     */
+    private Document createDocument() throws IOException {
+      File f = new File(pathToReport);
+      Document doc = getDocBuilder().newDocument();
+      Element rootElement = doc.createElement("testsuites");
+      doc.appendChild(rootElement);
+      addElementAttribute(doc, rootElement, "id", "1");
+      addElementAttribute(doc, rootElement, "failures", "0");
+      addElementAttribute(doc, rootElement, "tests", "0");
+      // Top level test suite
+      testSuite = addChildElement(doc, rootElement, "testsuite", "");
+      addElementAttribute(doc, testSuite, "id", "1");
+      addElementAttribute(doc, testSuite, "name", "OSSIndex");
 
-        String testCount = getTotalOfElementsByName("testcase").toString();
-        modifyElementAttribute("testsuites", 0, "tests", testCount);
-        String failureCount = getTotalOfElementsByName("failure").toString();
-        modifyElementAttribute("testsuites", 0, "failures", failureCount);
-
-        File path = new File(pathToReport).getAbsoluteFile();
-
-        if(parentDirIsWritable(path)) {
-            synchronized (fileLock) {// Make sure we are not reading and writing at the same time
-                TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                Transformer transformer = transformerFactory.newTransformer();
-                DOMSource source = new DOMSource(doc);
-                StreamResult result = new StreamResult(path);
-
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-
-                transformer.transform(source, result);
-            }
-        } else {
-            throw new IOException("Report (" + path + ") failed permissions check.");
-        }
+      writeDocument(doc);
+      return doc;
     }
 
-    private Element addChildElement(Element parent, String name, String data) {
-        Element elem = doc.createElement(name);
-        elem.appendChild(doc.createTextNode(data));
-        parent.appendChild(elem);
-        return elem;
+    /**
+     * Load the existing report file.
+     */
+    private Document loadDocument() {
+      Document doc = null;
+      try {
+        doc = getDocBuilder().parse(pathToReport);
+      }
+      catch (SAXException | IOException e) {
+        e.printStackTrace();
+        System.exit(1);
+      }
+      testSuite = (Element) doc.getElementsByTagName("testsuite").item(0);
+      return doc;
     }
 
-    private void addElementAttribute(Element parent, String name, String value) {
-        Attr attr = doc.createAttribute(name);
-        attr.setValue(value);
-        parent.setAttributeNode(attr);
+    private DocumentBuilder getDocBuilder() {
+      DocumentBuilder docBuilder = null;
+      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+      try {
+        docBuilder = docFactory.newDocumentBuilder();
+      }
+      catch (ParserConfigurationException e) {
+        e.printStackTrace();
+        System.exit(1);
+      }
+      return docBuilder;
     }
 
-    private  String buildFailureString(ArrayList<String> currentVulnerabilityList) {
-        String failureString = "";
-        for (String tmp: currentVulnerabilityList){
-            failureString = failureString + tmp + "\n";
-        }
-        return failureString.trim();
-    }
+    /**
+     * Write the report file
+     */
+    private void writeDocument(Document doc) throws IOException {
+      if (!parentDirIsWritable(new File(path))) {
+        throw new IOException(("Report directory is not writable: " + path));
+      }
+      try {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(pathToReport);
 
-    private Integer getTotalOfElementsByName(String name) {
-        return doc.getElementsByTagName(name).getLength();
-    }
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 
-    private void modifyElementAttribute(String tagName, Integer index, String attrName, String value) {
-        Node target = doc.getElementsByTagName(tagName).item(index);
-        NamedNodeMap attr = target.getAttributes();
-        Node nodeAttr = attr.getNamedItem(attrName);
-        nodeAttr.setTextContent(value);
+        transformer.transform(source, result);
+      }
+      catch (TransformerConfigurationException e) {
+        throw new IOException(e);
+      }
+      catch (TransformerException e) {
+        throw new IOException(e);
+      }
     }
+  }
 
-    private Boolean parentDirIsWritable(File path) throws IOException {
-        File parentDir = path.getParentFile();
-        if (!parentDir.exists()) {
-            parentDir.mkdirs();
-        }
-        if (parentDir.exists()) {
-            Set<PosixFilePermission> permissions = Files.getPosixFilePermissions(Paths.get(parentDir.getAbsolutePath()), LinkOption.NOFOLLOW_LINKS);
-            return (permissions.contains(PosixFilePermission.OTHERS_WRITE) ||
-                    permissions.contains(PosixFilePermission.GROUP_WRITE) ||
-                    permissions.contains(PosixFilePermission.OWNER_WRITE)
-                    );
-        }
-        return false;
+  private Boolean parentDirIsWritable(File path) throws IOException {
+    File parentDir = path.getParentFile();
+    if (!parentDir.exists()) {
+      parentDir.mkdirs();
     }
+    if (parentDir.exists()) {
+      Set<PosixFilePermission> permissions = Files
+          .getPosixFilePermissions(Paths.get(parentDir.getAbsolutePath()), LinkOption.NOFOLLOW_LINKS);
+      return (permissions.contains(PosixFilePermission.OTHERS_WRITE) ||
+          permissions.contains(PosixFilePermission.GROUP_WRITE) ||
+          permissions.contains(PosixFilePermission.OWNER_WRITE)
+      );
+    }
+    return false;
+  }
 }
 
